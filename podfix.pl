165b33745f70397ce457678fc0cacfdd669a0b87
Sam Roberts <vieuxtech@gmail.com>
2012-02-05 06:06:14 +0000

    fix pod links

--- podfix.pl
+++ podfix.pl
@@ -2,11 +2,5 @@
 #
 # QNX4 (<EM>http://www.qnx.com</EM>). O
 
-$root = "http://toronto.qenesis.com/Sam/";
-
-s#$root##g;
 s#(\S*)\s+\(<EM>(.*)</EM>\)#<A HREF="$2">$1</A>#g;
-
-#s/A<([^\|]*)\|(.*)>/<A href="">E<60>A>/g < vf.html
-
 
